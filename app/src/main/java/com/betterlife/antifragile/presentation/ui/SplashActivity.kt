5d5497742b15035ebcab72a11f558c55be7c8a22
= <=>
2024-07-08 15:16:10 +0900

    SWM-82 refactor: ui 패키지 추가 및 Activity 파일 위치 변경

--- app/src/main/java/com/betterlife/antifragile/presentation/ui/SplashActivity.kt
+++ app/src/main/java/com/betterlife/antifragile/presentation/ui/SplashActivity.kt
@@ -1,10 +1,11 @@
-package com.betterlife.antifragile
+package com.betterlife.antifragile.presentation.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.betterlife.antifragile.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
