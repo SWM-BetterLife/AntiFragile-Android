6aff501684a4001600882fca5783b197b7cdc4de
junseokkim <junsuk8960@naver.com>
2024-07-09 15:45:42 +0900

    SWM-90 feat: MainActivity 생성

---
+++ app/src/main/java/com/betterlife/antifragile/presentation/ui/main/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.betterlife.antifragile.presentation.ui.main
+
+import android.os.Bundle
+import com.betterlife.antifragile.databinding.ActivityMainBinding
+import com.betterlife.antifragile.presentation.base.BaseActivity
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
