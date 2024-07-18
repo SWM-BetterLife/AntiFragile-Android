26fea52a9238a0195707a1e9c070b91a252bd4d2
junseokkim <junsuk8960@naver.com>
2024-07-18 22:26:29 +0900

    SWM-95 feat: bottom navigation에 사용할 fragment 생성

---
+++ app/src/main/java/com/betterlife/antifragile/presentation/ui/mypage/MypageFragment.kt
@@ -0,0 +1,14 @@
+package com.betterlife.antifragile.presentation.ui.mypage
+
+import android.os.Bundle
+import android.view.View
+import com.betterlife.antifragile.R
+import com.betterlife.antifragile.databinding.FragmentMypageBinding
+import com.betterlife.antifragile.presentation.base.BaseFragment
+
+class MypageFragment : BaseFragment<FragmentMypageBinding>(R.layout.fragment_mypage) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
