c1d58ebe8c246430b163b15cf56829658505e125
junseokkim <junsuk8960@naver.com>
2024-07-19 16:47:27 +0900

    feat: baseurl 변경

--- app/src/main/java/com/betterlife/antifragile/presentation/util/Constants.kt
+++ app/src/main/java/com/betterlife/antifragile/presentation/util/Constants.kt
@@ -2,6 +2,6 @@ package com.betterlife.antifragile.presentation.util
 
 object Constants {
 
-    const val BASE_URL = "https://localhost:8080/"
+    const val BASE_URL = "http://43.203.255.48:8080"
 
 }
