268f833b075a8ebfe399358e5fea0b80a3417881
junseokkim <junsuk8960@naver.com>
2024-08-09 18:36:44 +0900

    SWM-115 feat: 일기 상세 뷰 감정티콘 테마 설정

--- app/src/main/java/com/betterlife/antifragile/data/model/common/Emotion.kt
+++ app/src/main/java/com/betterlife/antifragile/data/model/common/Emotion.kt
@@ -1,23 +1,29 @@
 package com.betterlife.antifragile.data.model.common
 
-enum class Emotion(val toKorean: String) {
-    JOY("기쁨"),
-    PASSION("열정"),
-    FLUTTER("설렘"),
-    NORMAL("평범함"),
-    AMAZEMENT("놀람"),
-    ANXIETY("불안"),
-    PANIC("당황"),
-    SAD("슬픔"),
-    PAIN("아픔"),
-    DEPRESSION("우울"),
-    JEALOUSY("질투"),
-    ENNUI("따분"),
-    FEAR("공포"),
-    ANGER("분노"),
-    FATIGUE("피곤"),
-    ERROR("오류"),
-    NOT_SELECTED("미선택");
+import com.betterlife.antifragile.R
+
+enum class Emotion(val toKorean: String, val bgRes: Int) {
+    JOY("기쁨", R.drawable.bg_emoticon_joy),
+    PASSION("열정", R.drawable.bg_emoticon_passion),
+    FLUTTER("설렘", R.drawable.bg_emoticon_flutter),
+    NORMAL("평범함", R.drawable.bg_emoticon_normal),
+    AMAZEMENT("놀람", R.drawable.bg_emoticon_amazement),
+    ANXIETY("불안", R.drawable.bg_emoticon_anxiety),
+    PANIC("당황", R.drawable.bg_emoticon_panic),
+    SAD("슬픔", R.drawable.bg_emoticon_sad),
+    PAIN("아픔", R.drawable.bg_emoticon_pain),
+    DEPRESSION("우울", R.drawable.bg_emoticon_depression),
+    JEALOUSY("질투", R.drawable.bg_emoticon_jealousy),
+    ENNUI("따분", R.drawable.bg_emoticon_ennui),
+    FEAR("공포", R.drawable.bg_emoticon_fear),
+    ANGER("분노", R.drawable.bg_emoticon_anger),
+    FATIGUE("피곤", R.drawable.bg_emoticon_fatigue),
+    ERROR("오류", R.color.white),
+    NOT_SELECTED("미선택", R.color.white);
+
+    fun getBackgroundResource(): Int {
+        return bgRes
+    }
 
     companion object {
         fun fromString(value: String?): Emotion {
