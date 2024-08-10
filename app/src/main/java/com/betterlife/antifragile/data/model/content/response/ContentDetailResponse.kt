9bdf7bafd28d10f4f9ca025a28835a3f6f994e35
junseokkim <junsuk8960@naver.com>
2024-08-10 17:06:00 +0900

    SWM-30: feat: 콘텐츠 상세조회 api response 수정 및 viewmodel 구현

--- app/src/main/java/com/betterlife/antifragile/data/model/content/response/ContentDetailResponse.kt
+++ app/src/main/java/com/betterlife/antifragile/data/model/content/response/ContentDetailResponse.kt
@@ -1,14 +1,12 @@
 package com.betterlife.antifragile.data.model.content.response
 
 data class ContentDetailResponse(
+    val id: String,
+    val title: String,
+    val description: String,
+    val channel: ChannelResponse,
     val url: String,
-    val appStats: AppStats,
+    val likeNumber: Long,
     val isLiked: Boolean,
     val isSaved: Boolean
-)
-
-data class AppStats(
-    val likeNumber: Long,
-    val viewNumber: Long,
-    val saveNumber: Long
 )
---
+++ app/src/main/java/com/betterlife/antifragile/presentation/ui/content/viewmodel/ContentDetailViewModel.kt
@@ -0,0 +1,30 @@
+package com.betterlife.antifragile.presentation.ui.content.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.betterlife.antifragile.data.model.base.BaseResponse
+import com.betterlife.antifragile.data.model.base.Status
+import com.betterlife.antifragile.data.model.content.response.ContentDetailResponse
+import com.betterlife.antifragile.data.repository.ContentRepository
+import kotlinx.coroutines.launch
+
+class ContentDetailViewModel(
+    private val contentRepository: ContentRepository
+) : ViewModel() {
+
+    private val _contentDetailResponse = MutableLiveData<BaseResponse<ContentDetailResponse>>()
+    val contentDetailResponse = _contentDetailResponse
+
+    init {
+        _contentDetailResponse.value = BaseResponse(Status.INIT, null, null)
+    }
+
+    fun getContentDetail(contentId: String) {
+        viewModelScope.launch {
+            _contentDetailResponse.value = BaseResponse(Status.LOADING, null, null)
+            val response = contentRepository.getContentDetail(contentId)
+            _contentDetailResponse.postValue(response)
+        }
+    }
+}
