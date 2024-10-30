9f7c2770e330eca14ae990398484150e45633b2e
songjeongmyeong <wjdaud13580@hanyang.ac.kr>
2024-10-30 15:35:20 +0900

    chore: 버전 업그레이드

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.betterlife.antifragile"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
